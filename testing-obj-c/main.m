65c3ce82c948939ed134b9be7b691c50ddb4cfe9
ANDRES KWAN <andreskwan@gmail.com>
2014-05-16 13:29:11 -0500

    Initial Commit

---
+++ testing-obj-c/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  testing-obj-c
+//
+//  Created by Andres Kwan on 5/16/14.
+//  Copyright (c) 2014 Kwan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
