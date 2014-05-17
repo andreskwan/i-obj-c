b1dc7fa2bfa62557120f86991a6e4dc6f9265847
ANDRES KWAN <andreskwan@gmail.com>
2014-05-17 07:42:45 -0500

    Dictionaries - created with literals

--- testing-obj-c/main.m
+++ testing-obj-c/main.m
@@ -21,6 +21,12 @@ int main(int argc, const char * argv[])
                                     @"k2",@"Obj2",
                                     @"k3",@"Obj3", nil];
         
+        //Literals - second way to create a dict
+        NSDictionary *dict = @{@"k1": @"o1",
+                               @"k2": @"o2",
+                               @"k3": @"o3",
+                               @"k4": @"o4"};
+        
     }
     return 0;
 }
