9d277bf1738f47977f0b48dc9ec476b216908f7b
ANDRES KWAN <andreskwan@gmail.com>
2014-05-17 07:47:18 -0500

    Dictionaries - query - subscript syntax

--- testing-obj-c/main.m
+++ testing-obj-c/main.m
@@ -16,16 +16,23 @@ int main(int argc, const char * argv[])
         // insert code here...
         NSLog(@"Hello, World!");
         
-        NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+        NSDictionary *dict1 = [NSDictionary dictionaryWithObjectsAndKeys:
                                     @"k1",@"Obj1",
                                     @"k2",@"Obj2",
                                     @"k3",@"Obj3", nil];
         
         //Literals - second way to create a dict
-        NSDictionary *dict = @{@"k1": @"o1",
+        NSDictionary *dict2 = @{@"k1": @"o1",
                                @"k2": @"o2",
                                @"k3": @"o3",
                                @"k4": @"o4"};
+        
+        //Querying
+        NSString *storedObj1 = [dict2 objectForKey:@"k3"];
+
+        //subscript syntax 
+        NSString *storedObj2 = dict2[@"k1"];
+        
         
     }
     return 0;
